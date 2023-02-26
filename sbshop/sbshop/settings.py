3d78c721c8f4f13ea5a3fe3b079feb51cb16c6cb
upendra17 <upendra.singh@happilyunmarried.com>
2023-02-26 17:56:08 +0530

    new changes

--- sbshop/sbshop/settings.py
+++ sbshop/sbshop/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-g7m@d819qy88@wf=+lvgl=t8xftaskiq-cvs8n6b*j_8_+hhe5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
